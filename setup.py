1a55659c582dc319e42eb67772f36f2fa4bababa
bentamar <abentamar@gmail.com>
2017-01-23 21:40:55 +0200

    Added a setup.py file, added an __init__ main file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="web_questionnaire",
+    packages=["web_questionnaire"],
+    include_package_data=True,
+    install_requires=[
+        "flask",
+        "email",
+        "smtplib"
+    ]
+)
