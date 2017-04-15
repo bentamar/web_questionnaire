4dbea206f21b1f3c4a9c52f6a54bc864b8ca9894
bentamar <abentamar@gmail.com>
2017-04-15 12:16:17 +0300

    Fixed really messy django files

--- wq_app/apps.py
+++ wq_app/apps.py
@@ -3,5 +3,5 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-class WebQuestionnaireConfig(AppConfig):
-    name = 'WebQuestionnaire'
+class WqAppConfig(AppConfig):
+    name = 'wq_app'
