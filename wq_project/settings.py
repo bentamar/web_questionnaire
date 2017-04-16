d391c59fcddabfbdad8d18a9846cbfd7b84a63d6
bentamar <abentamar@gmail.com>
2017-04-16 18:17:30 +0300

    Added the app to the INSTALLED_APPS config value

--- wq_project/settings.py
+++ wq_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'wq_app.apps.WqAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
